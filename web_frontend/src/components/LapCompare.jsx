fcc18c138f83c907a9c392f864508c91d21c5dc8
Kushan <dknathalage@gmail.com>
2021-05-27 21:28:08 +1000

    circuit picker

--- web_frontend/src/components/LapCompare.jsx
+++ web_frontend/src/components/LapCompare.jsx
@@ -1,21 +1,25 @@
-import React, {useState} from "react"
+import React, { useState, useEffect } from "react"
 import { LapPickerComponent } from "./LapPicker";
+import axios from 'axios'
 
 var mkey = 0;
+var yearList = []
 export function LapCompareComponent() {
+	useEffect(() => {axios.get("http://localhost:5000/api").then(res => {yearList = res.data})})
+
 	var [lapList, changeComponent] = useState([])
 
 	const handleRemoveLap = (event) => {
-		changeComponent([...lapList, lapList.splice(event.target.value)])
+		changeComponent([...lapList, lapList.splice(event.target.value, 1)])
 	}
 
 	const handleAddLap = (event) => {
 		event.preventDefault();
-		changeComponent([...lapList, 
-			<div key={mkey} className="card col">
-				<LapPickerComponent yearList={[2018, 2019, 2020, 2021]}/>
-				<button value={mkey}  className="btn btn-outline-secondary" onClick={handleRemoveLap}>Remove</button>
-			</div>
+		changeComponent([...lapList,
+		<div key={mkey} className="card col">
+			<LapPickerComponent yearList={yearList} />
+			<button value={mkey} className="btn btn-outline-secondary" onClick={handleRemoveLap}>Remove</button>
+		</div>
 		])
 		mkey++;
 	}
--- web_frontend/src/components/LapPicker.jsx
+++ web_frontend/src/components/LapPicker.jsx
@@ -1,49 +1,74 @@
 import React, { useState } from "react"
 import axios from "axios"
 
-function VenuePicker(year) {
-	return (
-		<>
-		</>
-	)
-}
+function VenuePickerComponent(props) {
+	var venueComponent = []
+	var [activeVenue, changeVenue] = useState();
+	console.log(props.venueList)
+	function renderVenues(){
+		var drivers = []
 
-function SessionPicker(year, venue) {
-	return (
-		<>
-		</>
-	)
-}
+		var placeholder = []
+		props.venueList.forEach(venue => {
 
-function DriverPicker(year, venue, session) {
-	return (
-		<>
-		</>
-	)
-}
+			placeholder.push(
+				<li
+					key={venue}
+					value={venue}
+					className={"list-group-item list-group-item-action"}>
+					{venue}
+				</li>
+			)
+		});
 
-function yearOnClick(e){
-	
+		return placeholder
+	}
+
+	return (
+		<ul className="list-group">
+			{renderVenues()}
+		</ul>)
 }
 
 export function LapPickerComponent(props) {
 
-	var placeholderYears = []
-	console.log(props.yearList)
-	for (var i = 0; i < props.yearList.length; i++) {
-		placeholderYears.push(
-			<li 
-				value={props.yearList[i]} 
-				onClick={yearOnClick}
-				className="list-group-item list-group-item-action">
-				{props.yearList[i]}
-			</li>
-		)
+	var [placeholderYears, updateYears] = useState([])
+	var [activeYear, activateYear] = useState(0)
+	var [venues, venueFetch] = useState([])
+	placeholderYears = props.yearList
+
+
+	// Rendering Year List
+	function renderYears() {
+		// OnClick action on a year
+		function yearOnClick(e) {
+			axios.get(`http://localhost:5000/api/${e.target.value}`).then(res => {
+				venues = res.data
+			})
+			activateYear(e.target.value)
+			updateYears([placeholderYears])
+		}
+
+		var placeholder = []
+		for (var i = 0; i < props.yearList.length; i++) {
+		
+			placeholder.push(
+				<li
+					key={props.yearList[i]}
+					value={props.yearList[i]}
+					onClick={yearOnClick}
+					className={activeYear == props.yearList[i] ? "list-group-item list-group-item-action active" : "list-group-item list-group-item-action"}>
+					{props.yearList[i]}
+					{activeYear == props.yearList[i] ? <VenuePickerComponent year={props.yearList[i]} venueList={venues}/> : <></>}
+
+				</li>)
+		}
+		return placeholder
 	}
 
 	return (
-		<>
-			{placeholderYears}
-		</>
+		<ul className="list-group">
+			{renderYears()}
+		</ul>
 	)
 }
